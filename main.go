75b4c8b44417507e2f836a774c6a99ef36cbfffd
Mauricio Ramirez <ramirezcalustrom@gmail.com>
2023-11-22 12:55:26 -0400

    Applid the embed package along with the ParseFS func from the templates package

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/Mauricio-3107/lenslocked-v2/controllers"
+	"github.com/Mauricio-3107/lenslocked-v2/templates"
 	"github.com/Mauricio-3107/lenslocked-v2/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,13 +15,13 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
-	r.Get("/galleries", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "galleries.gohtml")))))
+	r.Get("/galleries", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "galleries.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
