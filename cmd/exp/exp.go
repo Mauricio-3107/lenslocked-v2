f5d7a720078ac59fb28f3ed0289c8529967cb2a8
Mauricio Ramirez <ramirezcalustrom@gmail.com>
2023-12-01 20:38:25 -0400

    Half password reset

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	stdCtx "context"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-	"github.com/Mauricio-3107/lenslocked-v2/context"
 	"github.com/Mauricio-3107/lenslocked-v2/models"
+	"github.com/joho/godotenv"
+)
+
+const (
+	host     = "sandbox.smtp.mailtrap.io"
+	port     = 587
+	username = "ffadde926c3b46"
+	password = "8cbaa1078447b5"
 )
 
 func main() {
-	ctx := stdCtx.Background()
-
-	user := models.User{
-		Email: "mau@mauri.com",
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic(err)
+	}
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+	es := models.NewEmailService(models.SMTPCongif{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+	})
+	err = es.ForgotPassword("mauro@m.com", "lenslocked.com/reset-ps?reseturl=abc123")
+	if err != nil {
+		panic(err)
 	}
 
-	ctx = context.WithUser(ctx, &user)
-	retrievedUser := context.User(ctx)
-	fmt.Println(retrievedUser.Email)
+	fmt.Println("Email sent.")
 }
